e99c71f76c0d35374ea5a63b3af76fbc547b591c
JethroSama <kel73896@gmail.com>
2021-04-22 16:01:34 +0800

    User schema: password hash and validate, add package: bcryptjs

---
+++ src/api/models/User.ts
@@ -0,0 +1,58 @@
+import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends Document {
+	firstName: string;
+	lastName: string;
+	email: string;
+	hashedPassword: string;
+	isModerator: string;
+	role: string;
+	isDeleted: string;
+}
+
+const userSchema: Schema = new Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: false,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		hashedPassword: {
+			type: String,
+			required: true,
+		},
+		isModerator: {
+			type: Number,
+			default: 0,
+		},
+		role: {
+			type: Number,
+			default: 0,
+		},
+		isDeleted: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+userSchema
+	.virtual('password')
+	.set(function (this: IUser, password: string) {
+		this.hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+	})
+	.get(() => {});
+userSchema.methods.validPassword = function (this: any, password: string) {
+	return bcrypt.compareSync(password, this.hashedPassword);
+};
+
+export default model<IUser>('User', userSchema);
